f0ee667d77598f68045f5ab528540bdd47df840c
Jose Lee <jose.lee@conmio.com>
2014-04-13 20:37:51 +0300

    Initial start

---
+++ Assets/scripts/UserInput.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UserInput : MonoBehaviour {
+
+	void Start ()
+	{
+	
+	}
+
+	void Update ()
+	{
+		if(Input.GetMouseButtonDown(0))
+		{
+			print("add force");
+			//rigidbody.AddForce(0, 10, 0);
+		}
+	}
+}
