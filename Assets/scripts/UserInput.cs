f37ad5635846366610bcd041ab173dddf884e749
Jose Lee <jose.lee@conmio.com>
2014-04-21 11:34:27 +0300

    User input to toss piggy. Click drag release throws piggy in taht direction and force

--- Assets/scripts/UserInput.cs
+++ Assets/scripts/UserInput.cs
@@ -3,16 +3,33 @@ using System.Collections;
 
 public class UserInput : MonoBehaviour {
 
+	public float xForceMultiplier = 50f;
+	public float yForceMultiplier = 200f;
+
+	private Camera MainCamera;
+	private Vector3 mouseDown = Vector3.zero;
+	private Vector3 mouseUp = Vector3.zero;
+
 	void Start ()
 	{
-	
+		MainCamera = Camera.main;
 	}
 
 	void Update ()
 	{
 		if(Input.GetMouseButtonDown(0))
 		{
-			rigidbody2D.AddForce(new Vector2(50,200));
+			mouseDown = MainCamera.ScreenToWorldPoint(Input.mousePosition);
+		}
+		if(Input.GetMouseButtonUp(0))
+		{
+			mouseUp = MainCamera.ScreenToWorldPoint(Input.mousePosition);
+
+			float xForce = (mouseUp.x - mouseDown.x) * xForceMultiplier;
+			float yForce = (mouseUp.y - mouseDown.y) * yForceMultiplier;
+			Vector2 force = new Vector2(xForce, yForce);
+			rigidbody2D.AddForce(force);
+			print(force);
 		}
 	}
 }
